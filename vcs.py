39c6bfc542f8af75c40a196522c871571d243a8f
skyfire <skyfireitdiy@hotmail.com>
2019-06-03 15:45:09 +0800

    vcs添加模板渲染

--- vcs.py
+++ vcs.py
@@ -1,12 +1,13 @@
 import subprocess
 import os
+from jinja2 import Template
 
 
 def get_code(vcs_config):
     if vcs_config["type"] == "git":
-        cmd = ["git", "clone", vcs_config["addr"]]
+        cmd = ["git", "clone", Template(vcs_config["addr"]).render(os.environ)]
     elif vcs_config["type"] == "svn":
-        cmd = ["svn", "export", vcs_config["addr"], "--username", vcs_config["username"],
-               "--password", vcs_config["password"], "--trust-server-cert-failures=unknown-ca", "--non-interactive"]
+        cmd = ["svn", "export", Template(vcs_config["addr"]).render(os.environ), "--username", Template(vcs_config["username"]).render(os.environ),
+               "--password", Template(vcs_config["password"]).render(os.environ), "--trust-server-cert-failures=unknown-ca", "--non-interactive"]
     ret = subprocess.check_call(cmd, shell=True, env=os.environ)
     return ret == 0
